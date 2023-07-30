f6ad6d18fde6ff332616895eb292b0b18047949b
miyazi777 <m71203@gmail.com>
2023-07-30 12:48:10 +0900

    cssを調整した

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 
 function App() {
-    // const results = useState<string[]>([])
+    const [selected, setSelected] = useState(0);
+
     const ref = useCallback((node: HTMLDivElement | null) => {
         node?.focus()
     }, [])
@@ -12,16 +13,52 @@ function App() {
         "test1",
         "test2",
         "test3",
-    ]
+        "test4",
+        "test5",
+        "test6",
+        "test7",
+    ];
+
+    const handleEnter = () => {
+        console.log('selected', results[selected]);
+        // Exec(results[selected]).then();
+    }
+
+    const handleQuit = () => {
+        console.log('quit');
+        // Quit();
+    }
+
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        const key = e.code;
+
+        switch (key) {
+            case 'Up':
+            case 'ArrowUp':
+                setSelected((selected - 1) < 0 ? results.length - 1 : selected - 1);
+                break;
+            case 'Down': 
+            case 'ArrowDown': 
+                setSelected((selected + 1) >= results.length ? 0 : selected + 1);
+                break;
+            case 'Enter':
+                handleEnter();
+                break;
+            case 'Escape':  
+                handleQuit();
+                break;
+        }
+    }
 
     return (
-        <div id="App">
+        <div id="App" onKeyDown={handleKeyDown}>
             <div id="word">
               <input type="text" ref={ref} />
             </div>
             <div id="results">
-                {results.map(result => (
-                    <div className="row">
+                {results.map((result, index) => (
+                    <div key={index} className={`row ${index === selected ? "selected" : ""}`}>
                         <div>{result}</div>
                     </div>
                 ))}
